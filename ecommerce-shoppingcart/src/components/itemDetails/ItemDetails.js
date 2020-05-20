42b29893865950f5bacaa140dd4ec0f9b1ab1834
KingKebs <kebogile1201@icloud.com>
2020-05-21 01:12:03 +0200

    udpate repo
    udpate repo

--- ecommerce-shoppingcart/src/components/itemDetails/ItemDetails.js
+++ ecommerce-shoppingcart/src/components/itemDetails/ItemDetails.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { Button, Collapse, Well, Media, Row, Col } from "react-bootstrap";
-
+import { Button, Collapse, Media, Row, Col } from "react-bootstrap";
+import jordan from "../../images/J-11-concord.jpg";
 export default class ItemDetails extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+
     this.state = {
       open: false,
     };
@@ -13,13 +14,36 @@ export default class ItemDetails extends Component {
     return (
       <div>
         <Button
-          className="item-button"
+          className="item-details-button"
           bsStyle="link"
           onClick={() => this.setState({ open: !this.state.open })}
         >
-          {this.state.open === false ? `See` : `Hide`} Item ItemDetails
+          {this.state.open === false ? `See` : `Hide`} item details{" "}
           {this.state.open === false ? `+` : `-`}
         </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Media>
+              <img width={100} height={100} src={jordan} alt="thumbnail" />
+              <Media.Body>
+                <p>
+                  Essentials by OFM ESS-3085 Racing Style Leather Gaming Chair,
+                  Red
+                </p>
+                <Row className="show-grid">
+                  <Col md={6}>
+                    <strong>{`ZAR${this.props.price}`}</strong>
+                    <br />
+                    <strong className="price-strike">{`ZAR${this.props.price}`}</strong>
+                  </Col>
+                  <Col md={6}>
+                    <strong>Qty: 1</strong>
+                  </Col>
+                </Row>
+              </Media.Body>
+            </Media>
+          </div>
+        </Collapse>
       </div>
     );
   }
